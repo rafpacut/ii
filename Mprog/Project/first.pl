5a614fe19c51b3a218146b3f5f7d7e8b4ea472f3
ravik122 <ravik1222@gmail.com>
2015-04-23 20:47:04 +0200

    Changed graph construct, path marking.

--- Mprog/Project/first.pl
+++ Mprog/Project/first.pl
@@ -1,21 +1,38 @@
-populate([], X_row, Y_dimension, Y_dimension):- !.
-populate([H|List],X_row, Y_dimension, CountY):-
-	H = data(position(X_row, CountY),Visited,parent_pos(Z, Z1)),
-	CountY1 is CountY + 1,
-	populate(List,X_row,  Y_dimension, CountY1).	
+pop([vertex(position(X,Y_dimension),Visited,parent_pos(ParentX,ParentY))], X, Y_dimension, Y_dimension):- !.
+pop([Head|Rest], X, Y_dimension, YAcc):-
+	%Head = (X, YAcc),
+	Head = vertex(position(X, YAcc), Visited, parent_pos(ParentX, ParentY)),
+	YAcc1 is YAcc + 1,
+	pop(Rest, X, Y_dimension, YAcc1).
+
+create_graph(X_dimension, Y_dimension, Graph):-
+	create_graph(X_dimension, Y_dimension, 1, Graph).
+
+create_graph(X_dimension, Y_dimension, X_dimension, [Array]):- 
+	pop(Array, X_dimension, Y_dimension, 1), !.
 
 
+create_graph(X_dimension, Y_dimension, XAcc, [Array|RestOfGraph]):-
+	pop(Array, XAcc, Y_dimension, 1),
+	XAcc1 is XAcc + 1,
+	create_graph(X_dimension, Y_dimension, XAcc1, RestOfGraph).
 
-array2d_constructor(X_dimension,Y_dimension, List2d):-
-	array2d_constructor(X_dimension, Y_dimension, 0, List2d).
 
-array2d_constructor(X,_,X,[]):- !.
-array2d_constructor(X,Y,Count,[A|B]) :-
-  X\=Count,
-  populate(A,Count,Y, 0),
-  Count1 is Count+1,
-  array2d_constructor(X,Y,Count1,B).
+get_elem(Elem, Graph, X, Y):-
+	get(Column, Graph, X ,1),
+	nonvar(Column),
+	!,
+	get(Elem, Column, Y, 1).
 
+get_elem(_,_,_,_):- !.
+
+get(Elem, [Elem|_], Position, Position):-!.
+
+get(Elem, [Head|Tail], Position, Acc):-
+	Acc1 is Acc + 1,
+	get(Elem, Tail, Position, Acc1).
+
+get(_, [], _, _):- !.
 
 % 2d array handling.
 % get_elem(Elem, List2d, X, Y) gets the i,j element of 2d array
@@ -24,20 +41,20 @@ array2d_constructor(X,Y,Count,[A|B]) :-
 % List2d - Two dimensional array to be searched.
 % X,Y - position in rows and columns respectively.
 
-get_elem(Elem, List2d, X, Y):-
-	get_elem(List1d, List2d, X), 
-	nonvar(List1d),
-	get_elem(Elem, List1d, Y), !.
-
-get_elem(_,_,_,_):-!.
-	
-
-get_elem(Elem,[Elem|_],0):- !.
-get_elem(Elem,[_|Tail],Iter):-
-	IterNext is Iter - 1,
-	get_elem(Elem,Tail,IterNext).
-
-get_elem(_, [], _):- !. % This should prevent from crashing when no such elem exists.
+%get_elem(Elem, List2d, X, Y):-
+%	get_elem(List1d, List2d, X), 
+%	nonvar(List1d),
+%	get_elem(Elem, List1d, Y), !.
+%
+%get_elem(_,_,_,_):-!.
+%	
+%
+%get_elem(Elem,[Elem|_],0):- !.
+%get_elem(Elem,[_|Tail],Iter):-
+%	IterNext is Iter - 1,
+%	get_elem(Elem,Tail,IterNext).
+%
+%get_elem(_, [], _):- !. % This should prevent from crashing when no such elem exists.
 
 % queue implementation using difference list
 queue_constructor(L-L).
@@ -54,25 +71,31 @@ dequeue(Elem, List-Last, NewList-Last) :-
 	List = [Elem|Rest],
 	NewList = Rest.
 
-path_reconstruction(Graph, data(position(_,_),_,parent_pos(X,Y)), PathAcc, Path):-
+path_reconstruction(Graph, vertex(position(_,_),_,parent_pos(X,Y)), PathAcc, Path):-
 	nonvar(X), nonvar(Y), !,
 	get_elem(Parent, Graph, X, Y),
 	path_reconstruction(Graph, Parent, [(X,Y)|PathAcc], Path).
 
 path_reconstruction(_,_, Path, Path):- !.
 		
-mark_path(Graph, [[]|_]):-!.
+mark_path(Graph, [], first):- !.
+mark_path(Graph, [(X,Y)|Rest], first):-
+	mark_path(Graph, Rest).
+
+mark_path(Graph, [[]|_]):-!. % Is it still necessary?
 mark_path(Graph, []):- !.
 mark_path(Graph, [(X,Y)|Rest]):-
 	get_elem(Vertex, Graph, X, Y),
 	mark_visited(Vertex),
 	mark_path(Graph, Rest).
 
+	%Take first vertex out of recent path to allow bfs to start from it.
+	%Is the first element of RecentPath also the first of Path?	
 
 
 bfs(X_dimension, Y_dimension, (X,Y), (X1,Y1),RecentPath, Path):-
-	array2d_constructor(X_dimension, Y_dimension, Graph),
-	mark_path(Graph, RecentPath),
+	create_graph(X_dimension, Y_dimension, Graph),
+	mark_path(Graph, RecentPath, first),
 	get_elem(StartVertex, Graph, X, Y),
 	get_elem(EndVertex, Graph, X1, Y1),
 	create_queue(Queue),
@@ -80,9 +103,10 @@ bfs(X_dimension, Y_dimension, (X,Y), (X1,Y1),RecentPath, Path):-
 
 
 
+
+
 loop(Graph, _, EndVertex, EndVertex, Path):-
-	!,
-	EndVertex = data(position(X,Y),_,parent_pos(_,_)),
+	EndVertex = vertex(position(X,Y),_,parent_pos(_,_)),
 	path_reconstruction(Graph, EndVertex, [(X,Y)], Path).
 
 loop(Graph, Queue, Vertex, EndVertex, Path):-
@@ -105,7 +129,7 @@ loop(Graph, [], Vertex, Endvertex, _):-
 	% That should start another permutation.
 %The seperate Vertex and EndVertex should remain, becouse first loop call
 %is loop(_, [], SomeVertex, SomePosssiblyOtherVertex).
-has_no_parent(data(position(_,_),_,parent_pos(X,Y))):-
+has_no_parent(vertex(position(_,_),_,parent_pos(X,Y))):-
 	var(X), var(Y).
 
 add_neighbour(Graph, Queue, Vertex, Parent,ProductQueue):-
@@ -126,7 +150,7 @@ add_neighbour(_, Queue, _,_, Queue).
 
 
 add_neighbours(Graph, Queue, Parent, ProductQueue):-
-	Parent = data(position(X,Y),_,parent_pos(_,_)), 
+	Parent = vertex(position(X,Y),_,parent_pos(_,_)), 
 
 	X1 is X + 1, Y1 is Y + 1,
 	X2 is X - 1, Y2 is Y - 1,	
@@ -145,13 +169,13 @@ add_neighbours(Graph, Queue, Parent, ProductQueue):-
 
 
 
-was_not_visited(data(position(_,_),Visited, parent_pos(_,_))):-
+was_not_visited(vertex(position(_,_),Visited, parent_pos(_,_))):-
 	var(Visited).
 
-set_parent(data(position(_,_),_, parent_pos(X,Y) ), data(position(X,Y),_,parent_pos(_,_))).
+set_parent(vertex(position(_,_),_, parent_pos(X,Y) ), vertex(position(X,Y),_,parent_pos(_,_))).
 
 
-mark_visited(data(position(_,_),Visited, parent_pos(_,_))):-
+mark_visited(vertex(position(_,_),Visited, parent_pos(_,_))):-
 	Visited = true.
 
 	
@@ -171,39 +195,16 @@ permute(List, [((X,Y),(X1,Y1))|Rest]):-
 
 
 
+pairs_to_points([], []):- !.
+pairs_to_points([((X,Y),(X1,Y1))|PairsList], [(X,Y),(X1,Y1)|PointsList]):-
+	pairs_to_points(PairsList, PointsList).
+
+
 solve(X_dimension, Y_dimension, ListaPar, Rozwiazanie):-
 	permute(ListaPar, PermutedPairsList),
-	find_pair_path(PermutedPairsList, X_dimension, Y_dimension, [],  Rozwiazanie).
+	find_pair_path(PermutedPairsList, X_dimension, Y_dimension, [],  Rozwiazanie),
+	!.
 	
 
 
-
-
-
-% printing 2d array for debugging purposes.
-% print_array(Array2d, Xdimension, Ydimension, X, Y).
-% X/Ydimension - wymiary tablicy
-% X/Y - iteratory, wywolywac z X=1, Y=1.
-print_array(Array, Xdimension, Ydimension, X, Ydimension):-
-	!,
-	get_elem(Elem, Array, X, Ydimension),
-	write(Elem),
-	nl,
-	X1 is X + 1,
-	X1 =< Xdimension,
-	print_array(Array, Xdimension, Ydimension, X1, 1).
-
-
-print_array(Array, Xdimension, Ydimension, X, Y):-
-	get_elem(Elem, Array, X, Y),
-	write(Elem),
-	Y1 is Y + 1,
-	print_array(Array, Xdimension, Ydimension, X, Y1).
-
-print_array(Array, Xdimension, Ydimension, Xdimension):-
-	print_array(Array, Xdimension, Ydimension, Xdimension, 1), !.
-
-
-print_array(Array, Xdimension, Ydimension, X):-
-	print_array(Array, Xdimension, Ydimension, X, 1).
 
